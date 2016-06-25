fb38b8f5f5a290c231a45a600f39758ad7d73e12
TunaTerror <sjoerd.wiarda97@gmail.com>
2016-06-25 23:03:57 +0200

    Import functionality

--- js/Actor.js
+++ js/Actor.js
@@ -1,12 +1,18 @@
 Actor = function(geometry,material,physObj) {
+	if( 'undefined' != typeof global ) {
+	this.position = new THREE.Vector3();
+	this.quaternion = new THREE.Quaternion();
+	} else {
 	THREE.Mesh.call( this,geometry,material );
+	}
 	this.Collision = physObj;
 	this.Collision.parent = this;
 	this.type = 'Actor'
 }
-Actor.prototype = Object.create( THREE.Mesh.prototype );
-Actor.prototype.constructor  = Actor;
 
 if( 'undefined' != typeof global ) {
     module.exports = Actor;
+} else {
+	Actor.prototype = Object.create( THREE.Mesh.prototype );
+	Actor.prototype.constructor  = Actor;
 }
